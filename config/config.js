552dccc7d931787c0f602b6bd7f5ac3a55e721fd
Pierre <strobelpierre@gmail.com>
2018-04-05 17:21:54 +0200

    First Commit

---
+++ config/config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const rootPath = path.normalize(__dirname + '/..');
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'chatnode'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mysql://localhost/chatnode-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'chatnode'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mysql://localhost/chatnode-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'chatnode'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mysql://localhost/chatnode-production'
+  }
+};
+
+module.exports = config[env];
